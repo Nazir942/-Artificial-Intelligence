25875f7250e90f39783238f7ff5d3d81d53f4881
Md Nazir Ahmed <durjoy.nazir942@gmail.com>
2025-02-10 20:57:41 +0600

    Delete 4_second_higest.py

--- 4_second_higest.py
+++
@@ -1,9 +0,0 @@
-numbers = {3, 7, 2, 9, 5, 9, 3}
-
-unique_numbers = list(numbers)
-
-unique_numbers.sort(reverse=True)
-
-second_highest = unique_numbers[1]
-
-print("The second highest number is:", second_highest)
