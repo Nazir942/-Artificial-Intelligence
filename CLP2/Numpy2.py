cc7a7a7d459660ce9a6e54b9dafe15d6ea94f6b5
Md Nazir Ahmed <durjoy.nazir942@gmail.com>
2025-02-10 21:05:50 +0600

    Numpy2.py

--- CLP2/Numpy2.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-x = np.random.randint(0, 100, size=100)
-
-normalized = (x - x.min()) / (x.max() - x.min())
-
-print("Original Array:\n", x)
-print("\nNormalized Array:\n", normalized)
